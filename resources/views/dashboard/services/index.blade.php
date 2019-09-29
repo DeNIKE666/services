512fb2b8d431c1a781734abd57b57c14845ecb61
DeNIKE <wotdenike@gmail.com>
2019-09-29 20:14:00 +0300

    Update

--- resources/views/dashboard/services/index.blade.php
+++ resources/views/dashboard/services/index.blade.php
@@ -8,7 +8,21 @@
     <div class="row">
         <!-- Dashboard Box -->
         <div class="col-xl-12">
+
+            @isset($services)
+                <div class="margin-bottom-50">
+                    <select class="selectpicker" multiple data-selected-text-format="count > 1">
+                        <option adata-icon="icon-material-outline-assignment" selected>Assignment</option>
+                        <option data-icon="icon-material-outline-access-alarm">Alarm</option>
+                        <option data-icon="icon-material-outline-account-circle">Account</option>
+                        <option data-icon="icon-material-outline-cake">Cake</option>
+                    </select>
+                </div>
+            @endisset
+
+
             <div class="dashboard-box margin-top-0">
+
                 <!-- Content -->
                 <div class="content">
                     <!-- dashboard-box -->
